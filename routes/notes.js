627b47b96454977d9bca11b9146b97391be85110
Nisarg Patel <nhp0640@gmail.com>
2021-05-24 14:47:08 +0530

    created and modified time will come from application

--- routes/notes.js
+++ routes/notes.js
@@ -9,69 +9,84 @@ const router = express.Router();
 router.get("/getNotes", (req, res) => {
   Notes.find({ isDeleted: false })
     .then((result) => {
-      res.status(200).send({ status: 200, result: result, errorMessage: null });
+      res.status(200).send({ status: 200, result: result, message: null });
     })
     .catch((err) => {
-      res.status(500).send({ status: 500, result: null, errorMessage: err });
+      res.status(500).send({ status: 500, result: null, message: err });
     });
 });
 
 router.post("/addNote", async (req, res) => {
-  const currentTime = getCurrentTime();
-
   const note = new Notes({
     title: req.body.title,
     content: req.body.content,
-    createdAt: currentTime,
-    modifiedAt: currentTime,
+    createdAt: req.body.createdAt,
+    modifiedAt: req.body.modifiedAt,
   });
 
   let result;
   try {
     result = await note.save();
-    res.status(200).send({ status: 200, result: result, errorMessage: null });
+    res
+      .status(200)
+      .send({
+        status: 200,
+        result: result,
+        message: "Note added successfully",
+      });
   } catch (err) {
-    res.status(500).send({ status: 500, result: null, errorMessage: err });
+    res.status(500).send({ status: 500, result: null, message: err });
   }
 });
 
 router.put("/updateNote", (req, res) => {
-  const currentTime = getCurrentTime();
-
-  Notes.findOneAndUpdate(
-    { _id: req.body.noteId },
-    { ...req.body, modifiedAt: currentTime }
-  )
+  Notes.findOneAndUpdate({ _id: req.body.noteId }, { ...req.body })
     .then((result) => {
-      res.status(200).send({ status: 200, result: result, errorMessage: null });
+      res
+        .status(200)
+        .send({
+          status: 200,
+          result: result,
+          message: "Note updated successfully",
+        });
     })
     .catch((err) => {
-      res.status(500).send({ status: 500, result: null, errorMessage: err });
+      res.status(500).send({ status: 500, result: null, message: err });
     });
 });
 
 router.put("/archiveNote", (req, res) => {
-  const currentTime = getCurrentTime();
-
   Notes.findOneAndUpdate(
     { _id: req.body.noteId },
-    { isDeleted: true, modifiedAt: currentTime }
+    { isDeleted: true, modifiedAt: req.body.modifiedAt }
   )
     .then((result) => {
-      res.status(200).send({ status: 200, result: result, errorMessage: null });
+      res
+        .status(200)
+        .send({
+          status: 200,
+          result: result,
+          message: "Note archived successfully",
+        });
     })
     .catch((err) => {
-      res.status(500).send({ status: 500, result: null, errorMessage: err });
+      res.status(500).send({ status: 500, result: null, message: err });
     });
 });
 
 router.put("/deleteNote", (req, res) => {
   Notes.findOneAndDelete({ _id: req.body.noteId })
     .then((result) => {
-      res.status(200).send({ status: 200, result: result, errorMessage: null });
+      res
+        .status(200)
+        .send({
+          status: 200,
+          result: result,
+          message: "Note deleted successfully",
+        });
     })
     .catch((err) => {
-      res.status(500).send({ status: 500, result: null, errorMessage: err });
+      res.status(500).send({ status: 500, result: null, message: err });
     });
 });
 
